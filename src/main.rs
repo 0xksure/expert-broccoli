1694405cfa907a108beb446063a2da12f074ddde
kristohberg <kristoffer.hovland.berg@gmail.com>
2021-05-16 00:07:39 +0200

    Custom web error

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,24 @@
 extern crate rocket;
 use rand;
 
+mod weberror;
+use weberror::web_error::Error;
+
+fn generate_a_random_number(min: f32,max:f32)-> Result<i32,Error>{
+    if max <min{
+        return Err(Error::new("InternalServerError", "max has to be greater than min"));
+    }
+    let random_number = rand::random::<f32>()*(max-min)+min;
+    Ok(random_number as i32)
+}
+
 #[get("/random")]
 fn random_number() -> String {
-    let random_number = rand::random::<u8>().to_string();
-    format!("{{ 'random_n umber': {}' }}", random_number)
+    let random_number = match generate_a_random_number(22.0, 20.0){
+        Ok(num) => num,
+        Err(err) => return format!("{}",err)
+    };
+    format!("{{ 'random number': {}' }}", random_number)
 }
 
 fn main() {
